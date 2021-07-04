68804128d3a9ed2ec157ecee57ed89f62b08e2c5
wellingtonaquino <wellingtonm.aquino@gmail.com>
2021-07-04 18:55:26 -0300

    Atualizado botoes e Inicializado eventos

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+const contador = document.querySelector('#contador')
+const btnIniciar = document.querySelector('#btnIniciar')
+const btnPausar = document.querySelector('#btnPausar')
+const btnParar = document.querySelector('#btnParar')
+
+btnIniciar.addEventListener('click', () => {
+  
+})
+
+btnPausar.addEventListener('click', () => {
+
+})
+
+btnParar.addEventListener('click', () => {
+
+})
+
