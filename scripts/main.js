9263c348ac3c6ea23400f51c951d50e6bc67dccd
wellingtonaquino <wellingtonm.aquino@gmail.com>
2021-07-04 19:07:43 -0300

    Inicializando variaveis e Criando Iniciar e Pausar

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,35 @@ const btnPausar = document.querySelector('#btnPausar')
 const btnParar = document.querySelector('#btnParar')
 
 btnIniciar.addEventListener('click', () => {
-  
+    alterarContador()
 })
 
 btnPausar.addEventListener('click', () => {
-
+    alterarContador()
 })
 
 btnParar.addEventListener('click', () => {
-
+    alterarContadorr(true)
 })
 
+let tempoTotal = 1500
+let tempoAtual = 1500
+let tempoIntervalo = 300
+
+let contadorRodando = false
+
+const alterarContador = (reset) => {
+    if(reset){
+        
+    }else{
+        if (contadorRodando === true){
+            clearInterval(contadorValor)
+            contadorRodando = false
+        }else{
+            contadorRodando = true
+            contadorValor = setInterval(() => {
+                tempoAtual--
+            }, 1000)
+        }
+    }
+}
